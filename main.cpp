959bc98e58bfb80d6163386bd252e76df2308314
ambiesoft <ambiesoft.trueff@gmail.com>
2020-07-10 16:08:15 +0900

    add libs

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <Windows.h>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+{
+	glfwInit();
+
+	return 0;
+}
